3567b4c2fb11da93e5586d6a2b488a2a064c17c9
sippanas <alavicius.lukas@gmail.com>
2022-10-18 19:10:44 +0300

    Removed MRTK

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.MixedReality.Toolkit;
-using Microsoft.MixedReality.Toolkit.SceneSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,8 +7,6 @@ public class MenuManager : MonoBehaviour
 {
     public void LoadGameScene()
     {
-        // IMixedRealitySceneSystem sceneSystem = MixedRealityToolkit.Instance.GetService<IMixedRealitySceneSystem>();
-        // await sceneSystem.LoadContent("GameScene", LoadSceneMode.Single);
         StartCoroutine(LoadGameSceneAsync());
         Debug.Log("Trying to load the scene?");
     }
