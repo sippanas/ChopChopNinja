44968e7e43f8ab4251b11a49f10f949fd59f6307
sippanas <alavicius.lukas@gmail.com>
2022-11-20 01:22:42 +0200

    Fully working music manager in menu

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,18 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private AudioSource source;
+
+    [SerializeField]
+    private TextMeshProUGUI SongText;
+
+    [SerializeField]
+    private List<AudioClip> songs;
+
     void Start()
     {
-        
+        PlayNextSong();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayNextSong()
     {
-        
+        AudioClip currentSong = null;
+        foreach(AudioClip song in songs)
+        {
+            if(song == source.clip)
+            {
+                currentSong = song;
+            }
+        }
+
+        var nextSong = songs[(songs.IndexOf(currentSong) + 1) % songs.Count];
+
+        source.clip = nextSong;
+        SongText.text = $"{source.clip.name}";
+        source.Play();
     }
 }
